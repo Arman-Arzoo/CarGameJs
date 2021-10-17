aa243b8393fb76c1b86b164ac5799d20360b2221
Arman Ali <51259303+Arman-Arzoo@users.noreply.github.com>
2021-10-17 12:37:05 +0500

    keypress identify event

--- carGame.js
+++ carGame.js
@@ -3,3 +3,17 @@ const startScreen = document.getElementById("startScreen");
 const gameArea = document.getElementById("gameArea");
 
 console.log(score);
+
+const keypress = document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
+
+function keyDown(e) {
+  e.preventDefault();
+
+  const textdata = document.getElementById("test");
+  textdata.innerHTML += e.key;
+}
+
+function keyUp(e) {
+  e.preventDefault();
+}

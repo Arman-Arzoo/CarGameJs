ae8bee7b739c979f537492cfc78b5becdfebb97c
Arman Ali <51259303+Arman-Arzoo@users.noreply.github.com>
2021-10-17 14:43:48 +0500

    comment code and start game funtion

--- carGame.js
+++ carGame.js
@@ -1,25 +1,37 @@
+// initialize and decalare varialbles
 const score = document.getElementById("score");
 const startScreen = document.getElementById("startScreen");
 const gameArea = document.getElementById("gameArea");
 
-const keys = {
+// start game event
+startScreen.addEventListener("click", start);
+
+// select specific keys using object
+let keys = {
   ArrowUp: false,
   ArrowDown: false,
   ArrowRight: false,
   ArrowLeft: false,
 };
 
-const keypress = document.addEventListener("keydown", keyDown);
+// addEventListener to identify key press on page
+document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
 
 function keyDown(e) {
   e.preventDefault();
-  console.log(e.key);
   keys[e.key] = true;
-  console.log(keys);
 }
 
 function keyUp(e) {
   e.preventDefault();
   keys[e.key] = false;
+}
+
+// start the game
+function starGame() {
+  console.log("game playing");
+}
+function start() {
+  window.requestAnimationFrame(starGame);
 }

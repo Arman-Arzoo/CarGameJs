168d38a85dcce71b523d1a0c2d557442859edfae
Arman Ali <51259303+Arman-Arzoo@users.noreply.github.com>
2021-10-17 12:15:43 +0500

    assign id and make variable in cargame.js

---
+++ carGame.js
@@ -0,0 +1,5 @@
+const score = document.getElementById("score");
+const startScreen = document.getElementById("startScreen");
+const gameArea = document.getElementById("gameArea");
+
+console.log(score);
